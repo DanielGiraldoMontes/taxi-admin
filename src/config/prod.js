2dbba05d48aac260ca8e3b6a4cf98a0a61b25159
Daniel Giraldo <uniondigital@outlook.es>
2020-05-08 17:35:42 -0500

    First commit

---
+++ src/config/prod.js
@@ -0,0 +1,9 @@
+export const FirebaseConfig = {
+  apiKey: "AIzaSyAkYNuz1kegbe2XL49gIOSa_iVStTexSe0",
+  authDomain: "grabcab-461b2.firebaseapp.com",
+  databaseURL: "https://grabcab-461b2.firebaseio.com",
+  projectId: "grabcab-461b2",
+  storageBucket: "grabcab-461b2.appspot.com",
+  messagingSenderId: "546999524745",
+  appId: "1:546999524745:web:619a77c551df4a229f9558"
+};
